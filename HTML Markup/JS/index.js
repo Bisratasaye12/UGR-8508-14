8977e003258e28ff51430b0bfdab89e8eab25dcc
bisratplus <104831783+bisratplus@users.noreply.github.com>
2023-12-25 01:08:23 +0300

    second draft

--- HTML Markup/JS/index.js
+++ HTML Markup/JS/index.js
@@ -1,8 +1,29 @@
-const navToggler = document.getElementById('toggler');
-const navMenu = document.getElementById('main-nav');
 
 
 
-navToggler.addEventListener('click', () => {
-    navMenu.classList.toggle('d-none');
-});
+var slideIndex = 1;
+//showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n, slides_id) {
+showSlides(slideIndex += n, slides_id);
+}
+
+
+
+function showSlides(n, slides_id) {
+var i;
+var slides = document.getElementsByClassName(slides_id);
+
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+
+slides[slideIndex-1].style.display = "flex";
+
+
+}
+
+
